fbb43b32c03f9f9057828256d1db4e4fdd84266d
田原崇 <taharatakashi@taharatakashinoMacBook-Air.local>
2021-02-02 08:55:11 +0900

    CSVデータ作成画面

--- public_html/cgi-bin/flask_hosting.py
+++ public_html/cgi-bin/flask_hosting.py
@@ -1,9 +1,149 @@
-import sys, traceback, json, csv
+import sys, traceback, json, csv, shutil
 from flask import Flask, request, render_template, json
 from werkzeug.exceptions import HTTPException
-from datetime import date
+from datetime import date, timedelta
 
 server = Flask(__name__)
+
+@server.route("/create_csv", methods=['GET', 'POST'])
+def create_csv():
+    outputs = None
+    try:
+        #
+        # Method: GET
+        # CSVデータ作成画面
+        #
+        #
+        if request.method == 'GET':
+            html_json = {}
+            today = date.today()
+            yesterday = today - timedelta(days=1)
+
+            real_datas = []
+            with open('saved_data/real_data.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile, skipinitialspace=True)
+                for row in reader:
+                    real_datas.append(row)
+
+            if yesterday.isoformat() == real_datas[-1]['date']:
+                html_json['_8_38'] = real_datas[-1]['8:38']
+                html_json['_9_26'] = real_datas[-1]['9:26']
+                html_json['_9_30'] = real_datas[-1]['9:30']
+            html_json['flag'] = real_datas[-1]['flag']
+            html_json['message'] = ''
+
+            return render_template('create_csv.html', html_json=html_json)
+        #
+        # Method: POST
+        # CSVデータ登録
+        #
+        #
+        elif request.method == 'POST':
+            today = date.today()
+            yesterday = today - timedelta(days=1)
+            csv_columns = ["date", "Y開", "Y終", "Y活", "Yx", "Yn", "B5", "B開", "B終", "8:38", "9:26", "flag", "9:30", "活度", "max", "min", "down", "終値", "5日差", "type"]
+            inputs_cols = ["type", "_8_38", "_9_26", "_9_30", "_10_00", "_13_30", "_16_00", "max", "min"]
+
+            real_datas = []
+            html_json = {}
+            html_json['message'] = ''
+            form_dict = request.form.to_dict()
+            form_data = {}
+            real_data = {}
+            with open('saved_data/real_data.csv', newline='', encoding="utf-8") as csvfile:
+                reader = csv.DictReader(csvfile, skipinitialspace=True)
+                for row in reader:
+                    real_datas.append(row)
+
+            if form_dict['ticker'] != 'zm' or yesterday.isoformat() != form_dict['pass']:
+                if form_dict['ticker'] != 'zm':
+                    html_json['message'] = 'zm以外は現在実装中です。'
+                else:
+                    html_json['message'] = 'パスコードが一致しません。'
+            try:
+                for key, value in form_dict.items():
+                    outputs = value
+                    if key in inputs_cols:
+                        form_data[key] = float(value)
+            except ValueError:
+                html_json['message'] = '{}: 少数点第一位までの数値で入力してください。'.format(outputs)
+            if html_json['message'] != '':
+                # データ初期化して再表示
+                if yesterday.isoformat() == real_datas[-1]['date']:
+                    html_json['_8_38'] = real_datas[-1]['8:38']
+                    html_json['_9_26'] = real_datas[-1]['9:26']
+                    html_json['_9_30'] = real_datas[-1]['9:30']
+                html_json['flag'] = real_datas[-1]['flag']
+                return render_template('create_csv.html', html_json=html_json)
+
+            prev_row = -1
+            if yesterday.isoformat() == real_datas[-1]['date']:
+                real_data = real_datas[-1]
+                prev_row = -2
+            day2ago = prev_row - 1
+            day3ago = prev_row - 2
+            day4ago = prev_row - 3
+            day5ago = prev_row - 4
+            day6ago = prev_row - 5
+            real_data['date'] = yesterday.isoformat()
+            real_data['Y開'] = real_datas[prev_row]['9:30']
+            real_data['Y終'] = real_datas[prev_row]['終値']
+            real_data['Y活'] = real_datas[prev_row]['活度']
+            real_data['Yx'] = real_datas[prev_row]['max']
+            real_data['Yn'] = real_datas[prev_row]['min']
+            d1 = int(form_data['_16_00'] * 10)
+            d2 = int(float(real_datas[prev_row]['終値']) * 10)
+            d3 = int(float(real_datas[day2ago]['終値']) * 10)
+            d4 = int(float(real_datas[day3ago]['終値']) * 10)
+            d5 = int(float(real_datas[day4ago]['終値']) * 10)
+            d6 = int(float(real_datas[day5ago]['終値']) * 10)
+            d7 = int(float(real_datas[day6ago]['終値']) * 10)
+            real_data['B5'] = (d1 + d2 + d3 + d4 + d5) / 10
+            real_data['B開'] = real_datas[day2ago]['9:30']
+            real_data['B終'] = real_datas[day2ago]['終値']
+            real_data['8:38'] = form_data['_8_38']
+            real_data['9:26'] = form_data['_9_26']
+            real_data['flag'] = form_dict['flag']
+            real_data['9:30'] = form_data['_9_30']
+            real_data['活度'] = form_data['_10_00']
+            real_data['max'] = form_data['max']
+            real_data['min'] = form_data['min']
+            lowest = min(form_data['_9_30'], form_data['_10_00'], form_data['_13_30'], form_data['_16_00'])
+            real_data['down'] = (int(form_data['_9_26'] * 10) - int(lowest * 10)) / 10
+            real_data['終値'] = form_data['_16_00']
+            real_data['5日差'] = (d1 + d2 + d3 + d4 + d5) / 10
+            real_data['type'] = int(form_data['type'])
+            shutil.copyfile('saved_data/real_data_backup.csv', 'saved_data/real_data_backup2.csv')
+            shutil.copyfile('saved_data/real_data.csv', 'saved_data/real_data_backup.csv')
+            with open('saved_data/real_data.csv', 'w', newline='', encoding="utf-8") as csvfile:
+                writer = csv.DictWriter(csvfile, quoting=csv.QUOTE_ALL, fieldnames=csv_columns)
+                writer.writeheader()
+                for row in real_datas:
+                    if row['date'] != yesterday.isoformat():
+                        writer.writerow(row)
+                writer.writerow(real_data)
+            with open('saved_data/saved_form_data.txt', 'r', encoding="utf-8") as file:
+                json_str = file.readlines()
+                html_json = json.loads(json_str[0])
+                html_json['message'] = 'CSVデータの登録が完了しました。'
+            return render_template('index.html', html_json=html_json)
+
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
+        traceback.print_exception(exc_type, exc_value, exc_traceback,
+                        limit=2, file=sys.stdout)
+        return json.dumps({
+            "code": 0,
+            "name": "Unexpected Exception",
+            "description": "Server Error: {}".format(e),
+            "traceback": traceback.format_exc(),
+            #"outputs": outputs["_16_00"]
+        })
+    else:
+        pass
+    finally:
+        pass
 
 @server.route("/", methods=['GET', 'POST'])
 def index(html_json=None):
@@ -35,12 +175,13 @@ def index(html_json=None):
                         file.truncate(0)
                         file.seek(0)
                         file.writelines(json.dumps(html_json))
-
+            html_json['message'] = ''
             return render_template('index.html', html_json=html_json)
         elif request.method == 'POST':
+            html_json['message'] = ''
             form_dict = request.form.to_dict()
             if form_dict['ticker'] != 'zm':
-                with open('saved_data/saved_form_data.txt', 'r+', encoding="utf-8") as file:
+                with open('saved_data/saved_form_data.txt', 'w', encoding="utf-8") as file:
                     json_str = file.readlines()
                     html_json = json.loads(json_str[0])
                 return render_template('index.html', html_json=html_json)
@@ -84,7 +225,6 @@ def index(html_json=None):
                 stats_data['err_1000'] = float(stats_data['later_analyzed_1000']) - float(stats_data['real_1000'])
                 stats_data['err_lowest'] = float(stats_data['later_analyzed_lowest']) - float(stats_data['real_lowest'])
                 stats_data['date'] = stats_data['date'][5:].lstrip('0').replace('/0','/')
-
             return render_template('analyzed_result.html', stats=stats, analysis=analysis, inputs=html_json)
 
     except Exception as e:
@@ -95,7 +235,7 @@ def index(html_json=None):
         return json.dumps({
             "code": 0,
             "name": "Unexpected Exception",
-            "description": "Server Error",
+            "description": "Server Error: {}".format(e),
         })
     else:
         pass
@@ -118,4 +258,3 @@ def handle_exception(e):
 
 if __name__ == "__main__":
     server.run()
-
